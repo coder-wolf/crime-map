e9af57f3c31585ced5b3be68c42be4c221e249c3
Ahammad Sabir <sabir.cse5.bu@gmail.com>
2026-07-04 06:29:01 +0600

    Switch map tiles to default OSM

--- app/MapView.tsx
+++ app/MapView.tsx
@@ -178,8 +178,8 @@ const MapView = React.memo(function MapView({
       style={{ height: '100%', width: '100%' }}
     >
       <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/">CARTO</a>'
-        url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
 
       <MapResizer />
